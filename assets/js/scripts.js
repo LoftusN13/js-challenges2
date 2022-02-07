cb8124f2281525844196bea4501281b6d1344cb3
LoftusN13 <loftus.niamh@yahoo.ie>
2022-02-07 17:39:30 +0000

    Function to check-off all checkboxes selected using the shift key

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,29 @@
 // Checkbox Challenge
 const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
 
+let lastChecked;
+
 function handleCheck(e) {
-    console.log(e);
+    //check if shift key was down
+    // AND that they are checking (rather than unchecking)
+    let inBetween = false;
+
+    if(e.shiftKey && this.checked) {
+        //loop over every checkbox
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if(checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log("starting to check inbetween");
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        });
+    }
+
+    lastChecked = this;
 }
 
-checkboxes.forEach(checkbox => checkbox.addEventListener('change', handleCheck));
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
