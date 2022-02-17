02f51d5ab990d5d698c8c0324860648c893d098b
LoftusN13 <loftus.niamh@yahoo.ie>
2022-02-17 16:55:24 +0000

    Create separate html and js files for Slide in to scroll section and function, update navlinks and adjust padding of scroll section

---
+++ assets/js/scroll.js
@@ -0,0 +1,24 @@
+// Slide In On Scroll Challenge
+const slideImages = document.querySelectorAll('.slide-in');
+
+//prevent scroll function running too many times causing lag
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+};
+
+function checkSlide(e) {
+    console.log('hello');
+}
+
+window.addEventListener('scroll', checkSlide);
